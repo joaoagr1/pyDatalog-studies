dd834c4c5b9b114e50569b5979dcac84c76a896c
joaoagr1 <joao.rollo1@gmail.com>
2024-11-06 21:40:32 -0300

    salvando

---
+++ provaPratica/ex2.py
@@ -0,0 +1,44 @@
+from pyDatalog import pyDatalog
+
+pyDatalog.create_terms('Funcionarios', 'Projetos', 'Departamentos', 'Alocacoes','TrabalhaEm','PertenceAoDepartamento','Funcionario','Projeto','X','Z','Departamento')
+
+
+with open('funcionarios.txt', 'r') as arquivo:
+    for linha in arquivo:
+        partes = linha.split(';')
+        nome = partes[0].strip()
+        departamento = partes[1].strip()
+        anosExperiencia = partes[2].strip()
+        +Funcionarios(nome, departamento, anosExperiencia)
+
+with open('projetos.txt', 'r') as arquivo:
+    for linha in arquivo:
+        partes = linha.split(';')
+        nome = partes[0].strip()
+        departamentoResponsavel = partes[1].strip()
+        +Projetos(nome, departamentoResponsavel)
+
+
+with open('departamentos.txt', 'r') as arquivo:
+    for linha in arquivo:
+        partes = linha.split(';')
+        nome = partes[0].strip()
+        qtdFuncionarios = partes[1].strip()
+        +Departamentos(nome, qtdFuncionarios)
+
+with open('alocacoes.txt', 'r') as arquivo:
+    for linha in arquivo:
+        partes = linha.split(';')
+        nomeFuncionario = partes[0].strip()
+        nomeProjeto = partes[1].strip()
+        +Alocacoes(nomeFuncionario, nomeProjeto)
+
+
+
+TrabalhaEm(Funcionario, Projeto) <= (Funcionarios(Funcionario, X, Z) & Projetos(Projeto, X)) #type: ignore
+
+PertenceAoDepartamento(Funcionario, Departamento) <= (Funcionarios(Funcionario,X,Z) & Departamento(X,Z))
+
+funcionariosProjetos = TrabalhaEm(Funcionario, Projeto)
+
+print(funcionariosProjetos)
